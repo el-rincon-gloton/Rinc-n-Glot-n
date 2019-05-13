dc409eeedafc2f66a93df9d00652f104bf4fbd1d
HelicopteroApache <44465497+HelicopteroApache@users.noreply.github.com>
2019-05-13 12:46:02 +0200

    Add files via upload
    Commit de ventas y gastos

---
+++ RinconGloton/src/modelos/Gastos.java
@@ -0,0 +1,47 @@
+/**
+ * Mario Toledo
+ */
+package norm;
+
+import java.time.LocalDate;
+
+public class Gastos {
+private String cod_gasto;
+private double Presupuesto;
+private double importe_gasto;
+private double resultado;
+private LocalDate fechas_gasto;
+
+
+
+public Gastos(String cod_gasto, double presupuesto, double importe_Gasto, double resultado, LocalDate fechas_gasto) {
+	super();
+	this.cod_gasto = cod_gasto;
+	Presupuesto = presupuesto;
+	this.importe_gasto = importe_gasto;
+	this.resultado = resultado;
+	this.fechas_gasto = fechas_gasto;
+}
+
+public double getPresupuesto() {
+	return Presupuesto;
+}
+
+public String getCod_gasto() {
+	return cod_gasto;
+}
+
+public double getResultado() {
+	return resultado;
+}
+
+public LocalDate getFechas_gasto() {
+	return fechas_gasto;
+}
+
+public double getImporte_gasto() {
+	return importe_gasto;
+}
+
+
+}
---
+++ RinconGloton/src/modelos/Ventas.java
@@ -0,0 +1,39 @@
+/**
+ * Mario Toledo
+ */
+package norm;
+
+import java.time.LocalDate;
+
+public class Ventas {
+private String Cod_Venta;
+private double Beneficio;
+private LocalDate fechaVenta;
+private String lugar;
+
+public Ventas(String cod_Venta, double beneficio, LocalDate fechaVenta, String lugar) {
+	super();
+	Cod_Venta = cod_Venta;
+	Beneficio = beneficio;
+	this.fechaVenta = fechaVenta;
+	this.lugar = lugar;
+}
+
+public String getCod_Venta() {
+	return Cod_Venta;
+}
+
+public double getBeneficio() {
+	return Beneficio;
+}
+
+public LocalDate getFechaVenta() {
+	return fechaVenta;
+}
+
+public String getLugar() {
+	return lugar;
+}
+
+
+}
